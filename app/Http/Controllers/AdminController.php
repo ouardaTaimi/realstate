7c73c4daf193a1de6e2005dc69cfe9d15d67bce8
ouarda taimi <ouardataimi123@gmail>
2025-03-13 14:56:00 +0000

    installation of template

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,6 +10,6 @@ class AdminController extends Controller
     public function AdminDashboard()
     {
 
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }
 }
--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -59,6 +59,18 @@ class LoginRequest extends FormRequest
         }
         Auth::login($user, $this->boolean('remember'));
         RateLimiter::clear($this->throttleKey());
+
+        // $this->ensureIsNotRateLimited();
+
+        // if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        //     RateLimiter::hit($this->throttleKey());
+
+        //     throw ValidationException::withMessages([
+        //         'email' => trans('auth.failed'),
+        //     ]);
+        // }
+
+        // RateLimiter::clear($this->throttleKey());
     }
 
     /**
